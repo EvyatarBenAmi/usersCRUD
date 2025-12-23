55774ccc71652a671b9d931f4c2337e7b87bb877
Evyatar_BenAmi <evyatar2091@gmail.com>
2025-12-23 10:15:07 +0200

    finish creat user

--- ctrls/usersC.js
+++ ctrls/usersC.js
@@ -1,10 +1,10 @@
-import { reader, write } from "../read.js"
+import { jsonToArr, write } from "../read.js"
 
 export { read, selectById, createUser }
 
 const read = async (req, res) => {
     try {
-        res.send(await reader())
+        res.send(await jsonToArr())
     } catch (error) {
         res.send(`Error`)
     }
@@ -13,7 +13,7 @@ const read = async (req, res) => {
 const selectById = async (req, res) => {
     try {
         const id = req.params.id
-        const data = await reader()
+        const data = await jsonToArr()
         const userId = data.find(user => user.id === Number(id))
         userId ? res.send(userId) : res.send(`ID not found `)
     } catch (error) {
@@ -24,13 +24,13 @@ const selectById = async (req, res) => {
 //    createUser יש שגיאה וצריך לעבור על 
 const createUser = async (req, res) => {
     try {
-        const data = await reader()
-        const newName = req.body.name
-        const id = data.length + 1
-        const newUser = { "id": id, "name": newName }
-        await write(newUser)
-        const newData = await reader()
-        res.send(newData)
+        const data = await jsonToArr()
+        // console.log(data);
+        const newUser = { "id": data.length + 1, "name": req.body.name }
+        data.push(newUser)
+        await write(data)
+        const newReadData = await jsonToArr()
+        res.send(newReadData)
     } catch (error) {
         res.send(error)
     }
--- read.js
+++ read.js
@@ -1,8 +1,8 @@
 import fs from "fs/promises";
 
-export async function reader(){
+export async function jsonToArr(){
     try {
-        const read = await fs.readFile("./users.json","utf-8",()=>{})
+        const read = await fs.readFile("./users.json","utf-8")
         const data = JSON.parse(read)
         return data
     } catch (error) {
@@ -12,7 +12,7 @@ export async function reader(){
 
 export async function write(data){
     try {
-        fs.writeFile("./users.json", data, 'utf8',()=>{})
+        await fs.writeFile("./users.json", JSON.stringify(data), 'utf8')
     } catch (error) {
         console.log (`Error:`, error)
     }
